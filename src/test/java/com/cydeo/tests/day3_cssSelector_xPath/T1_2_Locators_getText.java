df830558e0708cbf5fc6bf71a87a133b374bdd95
Iskandar <perevodchik51@gmail.com>
2022-05-12 22:47:47 +0300

    day3 - Locators, getText(), getAttribute()

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xPath/T1_2_Locators_getText.java
@@ -0,0 +1,48 @@
+package com.cydeo.tests.day3_cssSelector_xPath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T1_2_Locators_getText {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = WebDriverFactory.getDriver("edge");
+        driver.manage().window().maximize();
+
+        System.out.println("Test started");
+
+        driver.get("https://login1.nextbasecrm.com/");
+        WebElement userName = driver.findElement(By.className("login-inp"));
+        userName.sendKeys("Admin");
+
+        WebElement password = driver.findElement(By.name("USER_PASSWORD"));
+        password.sendKeys("admin");
+
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
+
+        WebElement errorMessage = driver.findElement(By.className("errortext"));
+
+        String expectedErrorMessage = "Incorrect login or password";
+        String actualErrorMessage = errorMessage.getText();
+
+        Thread.sleep(1500);
+
+        if (actualErrorMessage.equals(expectedErrorMessage)){
+            System.out.println("Negative test passed");
+        }else{
+            System.err.println("Negative test failed: " + errorMessage);
+        }
+
+
+
+
+        Thread.sleep(10000);
+        driver.quit();
+        System.out.println("Test finished");
+
+
+    }
+}
--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -11,6 +11,7 @@ package com.cydeo.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.sql.Driver;
@@ -27,6 +28,12 @@ public class WebDriverFactory {
 
             WebDriverManager.firefoxdriver().setup();
             return new FirefoxDriver();
+
+        } else if (browserType.equalsIgnoreCase("edge")) {
+
+            WebDriverManager.edgedriver().setup();
+            return new EdgeDriver();
+
         } else {
             System.out.println("Browser type does not exist or not currently supported");
             System.out.println("Driver null");
