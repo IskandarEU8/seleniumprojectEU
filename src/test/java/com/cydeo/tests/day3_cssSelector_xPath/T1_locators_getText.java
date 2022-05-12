9ef6cefd2ce5add0b70ab8c83145f7442ec09583
Iskandar <perevodchik51@gmail.com>
2022-05-12 22:42:24 +0300

    day3 - Locators, getText(), getAttribute()

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xPath/T1_locators_getText.java
@@ -0,0 +1,50 @@
+package com.cydeo.tests.day3_cssSelector_xPath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_locators_getText {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        System.out.println("Test started");
+
+        driver.get("https://login1.nextbasecrm.com/");
+        WebElement userName = driver.findElement(By.className("login-inp"));
+        userName.sendKeys("Admin");
+
+        WebElement password = driver.findElement(By.name("USER_PASSWORD"));
+        password.sendKeys("admin");
+
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
+
+        WebElement errorMessage = driver.findElement(By.className("errortext"));
+
+        String expectedErrorMessage = "Incorrect login or password";
+        String actualErrorMessage = errorMessage.getText();
+
+        Thread.sleep(1500);
+
+        if (actualErrorMessage.equals(expectedErrorMessage)){
+            System.out.println("Negative test passed");
+        }else{
+            System.err.println("Negative test failed: " + errorMessage);
+        }
+
+
+
+
+        Thread.sleep(10000);
+        driver.quit();
+        System.out.println("Test finished");
+
+
+    }
+}
---
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -0,0 +1,37 @@
+package com.cydeo.utilities;
+
+// Task: New method creation
+// Static method
+// Accepts String arg: browserType
+//  - This arg will determine what type of browser is opened
+//  - if 'chrome' passed --> it will open chrome browser
+//  - if 'firefox' passed --> it will open firefox browser
+// Return type: WebDriver
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Driver;
+
+public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserType){
+        if (browserType.equalsIgnoreCase("chrome")){
+
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        } else if (browserType.equalsIgnoreCase("firefox")){
+
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else {
+            System.out.println("Browser type does not exist or not currently supported");
+            System.out.println("Driver null");
+            return null;
+        }
+    }
+
+}
