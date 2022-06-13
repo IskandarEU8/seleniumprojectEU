4e53ef7881778361151e6891dc765843016b8bf0
Iskandar <perevodchik51@gmail.com>
2022-06-13 10:07:09 +0300

    day09 0- Driver.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -1,6 +1,13 @@
 package com.cydeo.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -9,25 +16,57 @@ public class Driver {
      so we are closing access to the object of this class from outside the class
      */
 
-    private Driver(){}
+    private Driver() {
+    }
 
         /*
         we make WebDriver private, because we want to close access from outside the class.
         We make it static because we will use it in a static method.
          */
 
-        private static WebDriver driver;
+    private static WebDriver driver;
 
-        /*
-        Create a re-usable utility method which will return same driver instance when we call it
-         */
-        public static WebDriver getDriver(){
-            if (driver == null){
+    /*
+    Create a re-usable utility method which will return same driver instance when we call it
+     */
+    public static WebDriver getDriver() {
+        if (driver == null) {
 
+                /*
+                     We read our browserType from configurationType from configuration.properties
+                     This way , can control which browser is opened from outside our code, from configuration.properties.
+                 */
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
             }
 
-            return driver;
         }
-
+        return driver;
     }
 }
+
