bce627c15b9edb419bbc031d3d9fcd0ee2cfdcbb
Iskandar <perevodchik51@gmail.com>
2022-06-13 10:11:44 +0300

    day09 0- Driver.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -38,6 +38,10 @@ public class Driver {
                  */
             String browserType = ConfigurationReader.getProperty("browser");
 
+            /*
+            Depending on the browserType that will be return from configuration.properties file
+            switch statement will determine the case, and open the matching browser
+             */
             switch (browserType) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
