90b42df733eb85a9719fa1f58097fe29a9b743ea
Iskandar <perevodchik51@gmail.com>
2022-06-14 17:26:42 +0300

    day10 - JavascriptExecutor

--- src/test/java/com/cydeo/tests/day10_Upload_Actions_JSexecutor/T4_JavascriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_Upload_Actions_JSexecutor/T4_JavascriptExecutor.java
@@ -18,13 +18,21 @@ public class T4_JavascriptExecutor {
 //        ((JavascriptExecutor)Driver.getDriver()).executeScript("window.scroll(0, 750)");
 
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
-        for (int i = 0; i < 10; i++) {
-            BrowserUtils.sleep(1.3);
-            js.executeScript("window.scroll(100,750)");
-        }
 
 
         // a. 750 pixels down 10 times
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,750)");
+        }
+
+
         // b. 750 pixels up 10 times
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,-750)");
+        }
+
+        Driver.quitDriver(2);
     }
 }
