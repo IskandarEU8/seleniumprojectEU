da92236766fb7c5f728b27aa2b5d6377e9aca3ea
Iskandar <perevodchik51@gmail.com>
2022-06-14 17:15:38 +0300

    day10 - JavascriptExecutor

--- src/test/java/com/cydeo/tests/day10_Upload_Actions_JSexecutor/T4_JavascriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_Upload_Actions_JSexecutor/T4_JavascriptExecutor.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day10_Upload_Actions_JSexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
@@ -14,8 +15,14 @@ public class T4_JavascriptExecutor {
         Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
 
         // 3. Use below JavaScript method and scroll
+//        ((JavascriptExecutor)Driver.getDriver()).executeScript("window.scroll(0, 750)");
+
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
-//        js
+        for (int i = 0; i < 10; i++) {
+            BrowserUtils.sleep(1.3);
+            js.executeScript("window.scroll(100,750)");
+        }
+
 
         // a. 750 pixels down 10 times
         // b. 750 pixels up 10 times
