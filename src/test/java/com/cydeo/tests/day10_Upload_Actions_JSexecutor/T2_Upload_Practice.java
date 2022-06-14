64c488f2399b5bf2a33810a5d94b1f0c379fd060
Iskandar <perevodchik51@gmail.com>
2022-06-14 12:37:37 +0300

    day10 - Registration Form + Java Faker

--- src/test/java/com/cydeo/tests/day10_Upload_Actions_JSexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_Upload_Actions_JSexecutor/T2_Upload_Practice.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day10_Upload_Actions_JSexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T2_Upload_Practice {
@@ -9,7 +13,28 @@ public class T2_Upload_Practice {
     @Test
     public void upload_test(){
         Driver.getDriver().get(ConfigurationReader.getProperty("upload_test_url"));
+        String imgPath = "C:\\Users\\iCan\\Downloads\\Safe Uploads\\fast-food.png";
 
+        WebElement fileUpload = Driver.getDriver().findElement(By.id("file-upload"));
+        BrowserUtils.sleep(1);
+
+        fileUpload.sendKeys(imgPath);
+
+        WebElement uploadBtn = Driver.getDriver().findElement(By.id("file-submit"));
+        uploadBtn.click();
+
+
+        WebElement actualMessageElement = Driver.getDriver().findElement(By.tagName("h3"));
+
+        String expectedMessage = "File Uploaded!";
+        String actualMessage = Driver.getDriver().findElement(By.tagName("h3")).getText();
+
+        Assert.assertEquals(actualMessage, expectedMessage);
+        Assert.assertTrue(actualMessageElement.isDisplayed());
+
+
+        Driver.quitDriver(2.5);
     }
 
 }
+//3.40.05
