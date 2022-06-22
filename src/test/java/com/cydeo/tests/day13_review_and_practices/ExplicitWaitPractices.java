3e62d5ef0f6be8426446214972b30c27a847ff34
Iskandar <perevodchik51@gmail.com>
2022-06-23 00:09:40 +0300

    day13 - Sync

--- src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day13_review_and_practices;
 
 import com.cydeo.pages.DynamicControlsPage;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -11,33 +12,39 @@ import org.testng.annotations.Test;
 
 public class ExplicitWaitPractices {
 
+    DynamicControlsPage dcPage;
+
     @BeforeMethod
-    public void setupMethod(){
+    public void setupMethod() {
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_controls");
+        dcPage = new DynamicControlsPage();
     }
 
     @AfterMethod
-    public void teardown(){
+    public void teardown() {
         Driver.quitDriver(1.5);
     }
 
     @Test
-    public void remove_button_test(){
+    public void remove_button_test() {
 
-        DynamicControlsPage dcp = new DynamicControlsPage();
         // 3.Click to "Remove" button
-        dcp.removeButton.click();
+        dcPage.removeButton.click();
         // 4.Wait until "loading bar" disappears
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
-        wait.until(ExpectedConditions.invisibilityOf(dcp.loadingBar));
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 5);
+        wait.until(ExpectedConditions.invisibilityOf(dcPage.loadingBar));
 
         // 5. Verify:
         // a. Checkbox is not displayed
-        Assert.assertFalse(dcp.checkbox.isDisplayed());
+        try {
+            Assert.assertTrue(!dcPage.checkbox.isDisplayed());
+            Assert.assertFalse(dcPage.checkbox.isDisplayed());
+        } catch (NoSuchElementException e) {
+            Assert.assertTrue(true);
+        }
         // b. "It's gone!" message is displayed.
-        Assert.assertTrue(dcp.message.isDisplayed());
-
-
+        Assert.assertTrue(dcPage.message.isDisplayed());
+        Assert.assertTrue(dcPage.message.getText().equals("It's gone!"));
 
 //13121
     }
