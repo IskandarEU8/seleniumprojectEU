9fda13947944dfde3e0fb7ff08528bc66b197b85
Iskandar <perevodchik51@gmail.com>
2022-06-22 19:50:42 +0300

    day13 - Sync

--- src/test/java/com/cydeo/tests/day12_PageObjectModel_Synchronization/synchronization/T4_DynamicallyLoadedPageElements7.java
+++ src/test/java/com/cydeo/tests/day12_PageObjectModel_Synchronization/synchronization/T4_DynamicallyLoadedPageElements7.java
@@ -4,6 +4,8 @@ import com.cydeo.utilities.Driver;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -21,20 +23,34 @@ public class T4_DynamicallyLoadedPageElements7 {
         Driver.quitDriver(2.3);
     }
 
+
     @Test
-    public void dynamically_loaded_page_elemets7_test() {
+    public void dynamically_loaded_page_elemets7_test() throws InterruptedException {
+//        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/7");
+        Thread.sleep(10);
         WebElement doneTextElement = Driver.getDriver().findElement(By.xpath("//div[@id='alert']"));
         String expectedDoneText = "Done!";
         String actualDoneText = doneTextElement.getText();
 
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
 //        Assert.assertEquals(actualDoneText, expectedDoneText);
 
         WebElement spoungeBobImage = Driver.getDriver().findElement(By.xpath("//img[@src='/img/a-few-moments.jpg']"));
+        wait.until(ExpectedConditions.visibilityOf(spoungeBobImage));
+        Assert.assertTrue(spoungeBobImage.isDisplayed());
 
 
 
     }
 
+    @Test
+    public void  custom_test(){
+        WebElement img = Driver.getDriver().findElement(By.tagName("img"));
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.visibilityOf(img));
+        Assert.assertTrue(img.isDisplayed());
+    }
 
 
 
--- src/test/java/com/cydeo/tests/day13_review_and_practices/T1_DoubleClick_Test.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/T1_DoubleClick_Test.java
@@ -1,6 +1,34 @@
 package com.cydeo.tests.day13_review_and_practices;
 
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
 public class T1_DoubleClick_Test {
+
+
+    @AfterMethod
+    public void tearDown(){
+        Driver.quitDriver(1.4);
+    }
+
+    @Test
+    public void t1_double_click_test(){
+
+        //1. Goto: https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2
+
+        Driver.getDriver().navigate().to("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2");
+
+        //2. Switch to iframe.
+
+        //3. Double-click on the text “Double-click me to change my text color.”
+
+        //4. Assert:Text’s “style” attribute value contains “red”.
+
+
+
+    }
+
 
 }
 //52.00.00
